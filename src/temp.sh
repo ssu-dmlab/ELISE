3e755d058cbbaf40980ad3cbfa2cff9dff7886d9
Minseo Joen <raichal2000@naver.com>
2024-12-27 13:35:14 +0000

    feat: reset layer == 1

--- src/temp.sh
+++
@@ -1,5 +0,0 @@
-python3 -m main --seed 273 --rank_ratio 0.4 --c 1.0 --num_layer 0 >> temp.txt
-python3 -m main --seed 588 --rank_ratio 0.4 --c 1.0 --num_layer 0 >> temp.txt
-python3 -m main --seed 600 --rank_ratio 0.4 --c 0.01 --num_layer 5 >> temp.txt
-python3 -m main --seed 681 --rank_ratio 0.4 --c 1.0 --num_layer 3 >> temp.txt
-python3 -m main --seed 846 --rank_ratio 0.3 --c 0.02 --num_layer 5 >> temp.txt
